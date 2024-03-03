e810b70e3170449f85e22b6a55fda0fabe5c1441
GaGman4ik <zaika.vas901@gmail.com>
2024-03-03 20:39:31 +0300

    Improved Design

--- GraphsPlotting/MainWindow.xaml.cs
+++ GraphsPlotting/MainWindow.xaml.cs
@@ -20,7 +20,7 @@ namespace GraphsPlotting
             // Парсим полученное выражение
             Parcer parser = new Parcer(TextBoxInput.Text);
             var result = parser.Parse();
-
+            if (result.Token == "Error") return;
             // Получим панель для рисования
             GraphPane pane = zgc.GraphPane;
 
@@ -31,11 +31,17 @@ namespace GraphsPlotting
             PointPairList list = new PointPairList();
 
             // Парсим границы отрисовки
-            double xmin = Double.Parse(TextBoxDownBound.Text);
-            double xmax = Double.Parse(TextBoxUpBound.Text);
+            double xmin = 0;
+            double xmax = 0;
+            if (!(double.TryParse(TextBoxDownBound.Text, out xmin) && double.TryParse(TextBoxUpBound.Text, out xmax)))
+            {
+                System.Windows.Forms.MessageBox.Show("Incorrect Bounds");
+                return;
+            }
+            
 
             // Заполняем список точек
-            for (double x = xmin; x <= xmax; x += (xmax - xmin)/100000)
+            for (double x = xmin; x < xmax; x += (xmax - xmin)/100000)
             {
                 // добавим в список точку
                 list.Add(x, parser.Calculate(result, x));
@@ -44,7 +50,7 @@ namespace GraphsPlotting
             // Создадим кривую с названием "Sinc",
             // которая будет рисоваться голубым цветом (Color.Blue),
             // Опорные точки выделяться не будут (SymbolType.None)
-            LineItem myCurve = pane.AddCurve("TextBoxInput.Text", list, System.Drawing.Color.Blue, SymbolType.None);
+            LineItem myCurve = pane.AddCurve(TextBoxInput.Text, list, System.Drawing.Color.Blue, SymbolType.None);
 
             // Вызываем метод AxisChange (), чтобы обновить данные об осях.
             // В противном случае на рисунке будет показана только часть графика,
@@ -69,6 +75,20 @@ namespace GraphsPlotting
         //    zgc.Text = text;
         //}
 
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string str = (string)((System.Windows.Controls.Button)e.OriginalSource).Content;
+
+            if (str == "Clear")
+            {
+                TextBoxInput.Text = "";
+            }
+            else
+            {
+                TextBoxInput.Text += str;
+            }
+
+        }
 
         private void TextBoxInput_TextChanged(object sender, TextChangedEventArgs e)
         {
@@ -79,5 +99,7 @@ namespace GraphsPlotting
         {
             
         }
+
+ 
     }
 }
